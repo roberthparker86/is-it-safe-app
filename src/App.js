66382f337f5f12bad90be763c6add98c01b2f0cc
roberthparker86 <roberthparker86@gmail.com>
2021-09-23 13:37:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+const Test = (props) => {
+	return <h1>{props.children}</h1>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Test>
+			Hippity hoppity flippity floppity.
+		</Test>
   );
 }
 
